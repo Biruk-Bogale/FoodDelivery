f2edea168da86484f45935e9dd225e964f63388b
unknown <you@example.combbuurraa10@gmail.com>
2024-07-11 13:37:06 +0300

    create restaurant form

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -21,13 +21,24 @@ function UsernameMenu() {
       <DropdownMenuContent>
         <DropdownMenuItem>
           <Link
+            to={"/manage-restaurant"}
+            className="font-bold hover:text-orange-500 px-12"
+          >
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem>
+          <Link
             to={"/user-profile"}
             className="font-bold hover:text-orange-500 px-12"
           >
             User Profile
           </Link>
         </DropdownMenuItem>
+
         <Separator />
+
         <DropdownMenuItem>
           <Button
             onClick={() => {
