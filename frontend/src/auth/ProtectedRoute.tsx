26928ec799532e2c45c538cef7a42c4f4e9cf643
unknown <you@example.combbuurraa10@gmail.com>
2024-07-17 10:04:22 +0300

    Pre-populate restaurant form

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,10 +1,25 @@
+import Layout from "@/layouts/layout";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoute() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-  return isAuthenticated ? <Outlet /> : <Navigate to={"/"} replace />;
+  if (isLoading) {
+    return (
+      <Layout>
+        <h1>Loading...</h1>
+      </Layout>
+    );
+  }
+
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
+
+  return <Navigate to={"/"} replace />;
+
+  // return isAuthenticated ? <Outlet /> : <Navigate to={"/"} replace />;
 }
 
 export default ProtectedRoute;
