b57ed6e6f3d18a82c2019d1fd460ac475c6d6e56
unknown <you@example.combbuurraa10@gmail.com>
2024-07-18 14:39:00 +0300

    search api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoute";
 import myRestaurantRoute from "./routes/myRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+
 import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
@@ -32,6 +34,7 @@ app.get("/health", async (req: Request, res: Response) => {
 // /api/my/user
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7777, () => {
   console.log("server started on localhost:7777");
