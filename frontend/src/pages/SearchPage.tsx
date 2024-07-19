51753e79f199cdce4e93ee7dfce6281ae3b69398
unknown <you@example.combbuurraa10@gmail.com>
2024-07-19 10:31:25 +0300

    search API request

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useSearchRestaurant } from "@/api/RestaurantApi";
+import { useParams } from "react-router-dom";
 
 function SearchPage() {
-    const {city} = useParams()
+  const { city } = useParams();
+  const { results } = useSearchRestaurant(city);
+
   return (
     <span>
-      user Serched for {city}
+      user Serched for {city + " "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName},{restaurant.city}
+          </span>
+        ))}
+      </span>
     </span>
-  )
+  );
 }
-
-export default SearchPage
+export default SearchPage;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -26,3 +26,12 @@ export type Restaurant = {
   imageFile: File;
   lastedUpdated: string;
 };
+
+export type RestaurantSearchResponse = {
+  data: Restaurant[];
+  pagination: {
+    total: number;
+    page: number;
+    pages: number;
+  };
+};
