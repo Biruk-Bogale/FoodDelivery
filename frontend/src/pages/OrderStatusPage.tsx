4c102498f3111af5270a5cc40169d5a9250f4f5a
unknown <you@example.combbuurraa10@gmail.com>
2024-09-04 17:12:07 +0300

    Order status config

--- frontend/src/pages/OrderStatusPage.tsx
+++ frontend/src/pages/OrderStatusPage.tsx
@@ -1,5 +1,7 @@
 import { useGetMyOrders } from "@/api/OrderApi";
+import OrderStatusDetail from "@/components/OrderStatusDetail";
 import OrderStatusHeader from "@/components/OrderStatusHeader";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 
 function OrderStatusPage() {
   const { orders, isLoading } = useGetMyOrders();
@@ -16,7 +18,16 @@ function OrderStatusPage() {
     <div className="space-y-10 ">
       {orders.map((order) => (
         <div className="space-y-10 bg-gray-50 p-10 rounded-lg">
-          <OrderStatusHeader order={order}/>
+          <OrderStatusHeader order={order} />
+          <div className="grid gap-10 md:grid-cols-2">
+            <OrderStatusDetail order={order} />
+            <AspectRatio ratio={16 / 7}>
+              <img
+                src={order.restaurant.imageUrl}
+                className="rounded-md object-cover h-full w-full"
+              />
+            </AspectRatio>
+          </div>
         </div>
       ))}
     </div>
