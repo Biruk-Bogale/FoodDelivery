07d1cf25023b69fac5656350a262d2bb92c05232
unknown <you@example.combbuurraa10@gmail.com>
2024-09-04 16:14:40 +0300

    Get customer orders Request

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -35,3 +35,31 @@ export type RestaurantSearchResponse = {
     pages: number;
   };
 };
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemsId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAT: string;
+  restaurantId: string;
+};
